274884ac1cd4be99244918850ade1c4aedf79b68
Grace <grace941217@gmail.com>
2022-10-01 22:09:41 -0400

    commit tree 257733344c368ec7ea25b0278c2f22b93593f927

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+  
+function destructivelyAppendCat(Ralph) {
+  cats.push(Ralph);
+  return cats;
+}
+
+function destructivelyPrependCat(Bob) {
+  cats.unshift(Bob);
+  return cats;
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+
+function appendCat(Broom) {
+  var tigers = [...cats, Broom];
+  return tigers;
+}
+
+function prependCat(Arnold) {
+  var kittens = [Arnold, ...cats];
+  return kittens;
+}
+
+function removeLastCat() {
+  var caats = cats.slice(0,cats.length-1);
+  return caats;
+}
+
+function removeFirstCat() {
+  var catts = cats.slice(1);
+  return catts
+}
+
